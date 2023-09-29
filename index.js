dabe76e470bdcc3786d4b42f1627bef85b03eebb
Aleksander White <zander1116@aol.com>
2023-09-29 16:45:57 -0700

    Added functionality to the keyboard. Textarea displays typed phrases and it can toggle the display

--- index.js
+++ index.js
@@ -21,13 +21,23 @@ const Keyboard = {
         this.elements.keysContainer = document.createElement('div');
 
         // Display main elements
-        this.elements.main.classList.add('keyboard', '1keyboard--hidden');
+        this.elements.main.classList.add('keyboard', 'keyboard--hidden');
         this.elements.keysContainer.classList.add('keyboard--keys');
         this.elements.keysContainer.appendChild(this._createKeys());
+        this.elements.keys = this.elements.keysContainer.querySelectorAll('.keyboard--key');
 
         // Add to DOM
         this.elements.main.appendChild(this.elements.keysContainer);
         document.body.appendChild(this.elements.main);
+
+        // Display keys in textarea
+        document.querySelectorAll('.use--keyboard--input').forEach(element => {
+            element.addEventListener('focus', () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
     },
 
     _createKeys() {
@@ -86,7 +96,7 @@ const Keyboard = {
                     break;
                 case 'spacebar':
                     keyElement.classList.add('keyboard--key--x-wide');
-                    keyElement.innerHTML = createIconHTML('keyboard_spacebar');
+                    keyElement.innerHTML = createIconHTML('space_bar');
                     keyElement.addEventListener('click', () => {
                         this.properties.value += ' ';
                         this._triggerEvent('oninput');
@@ -122,19 +132,33 @@ const Keyboard = {
     },
 
     _triggerEvent(handlerName) {
-        console.log(handlerName)
+        if(typeof this.eventHandlers[handlerName] == 'function') {
+            this.eventHandlers[handlerName](this.properties.value);
+        }
     },
 
     _toggleCapsLock() {
-        console.log("Caps")
+        this.properties.capsLock = !this.properties.capsLock;
+        for(const key of this.elements.keys) {
+            if(key.childElementCount === 0) {
+                key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+        }
+
     },
 
     open(initialValue, oninput, onclose) {
-
+        this.properties.value = initialValue || '';
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.remove('keyboard--hidden');
     },
 
     close() {
-
+        this.properties.value = '';
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.add('keyboard--hidden');
     }
 };
 
